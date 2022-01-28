a45b892a438b207638f27b5d81d081eeec7061db
naoyamaki <stringbassandsticks@gmail.com>
2022-01-29 08:57:55 +0900

    javaのバージョンを17に変更

--- app/spring-boot-src/src/main/java/com/example/Application.java
+++ app/spring-boot-src/src/main/java/com/example/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(ExampleApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
